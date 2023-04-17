a77269a948eac90f3555f313b45f4c58e7f414ec
SpaceDoddyssey <cameronradodd@gmail.com>
2023-04-17 13:47:21 -0700

    Remaining features

--- src/prefabs/Spaceship.js
+++ src/prefabs/Spaceship.js
@@ -4,7 +4,7 @@ class Spaceship extends Phaser.GameObjects.Sprite {
         super(scene, x, y, texture, frame);
         scene.add.existing(this);   // add to existing scene
         this.points = pointValue;   // store pointValue
-        this.moveSpeed = 3          // pixels per frame
+        this.moveSpeed = game.settings.spaceshipSpeed;        // pixels per frame
     }
 
     update() {
@@ -14,5 +14,10 @@ class Spaceship extends Phaser.GameObjects.Sprite {
         if(this.x <= 0 - this.width) {
             this.reset();
         }
+    }
+
+    // position reset
+    reset() {
+        this.x = game.config.width;
     }
 }
